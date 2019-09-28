12bde03cbca2d0c3806a234456d59b81e330587d
Daan Dirker <daan.development@gmail.com>
2019-09-28 21:04:34 +0200

    Redux store setup

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
+import React, { Component } from 'react'
 import { AppRegistry } from 'react-native'
 import { name as appName } from './App/Config/app.json'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 
+import {  createStore, combineReducers } from 'redux'
+import { Provide } from 'react-redux'
+
 import HomeScreen from './App/Containers/HomeScreen'
 import TransactionScreen from './App/Containers/TransactionScreen'
 
-const Navigator = createStackNavigator({
-  Home: { screen: HomeScreen },
-  Transaction: { screen: TransactionScreen }
-})
+const ScreensContainer = createAppContainer(
+  createStackNavigator({
+    Home: { screen: HomeScreen },
+    Transaction: { screen: TransactionScreen }
+  })
+)
 
-const App =  createAppContainer(Navigator)
+//TODO: Add reducers here once implemented
+// let store = createStore(combineReducers({
+// })) 
+
+class App extends Component {
+  render() {
+    return(
+      // <Provider store={store}>
+        <ScreensContainer/>
+      // </Provider>
+    )
+  }
+}
 
 AppRegistry.registerComponent(appName, () => App)
